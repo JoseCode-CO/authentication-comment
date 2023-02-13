26ed7506a8a6d3ae910b403f2ec6e26bc8bfe1e5
CODE WITH JOSE DAVID <48775594+JoseCode-CO@users.noreply.github.com>
2023-02-12 21:23:38 -0500

    Register

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password =Hash::make($request->password);
+
+        $user->save();
+
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+    }
+
+    public function logout()
+    {
+    }
+
+    public function allUsers()
+    {
+    }
+}
